f31bd7ba6efb267574cf3eac6328be2245d7cb2c
muhammed-ogz <muhammednabioguz@gmail.com>
2023-12-31 13:48:33 +0300

    classes added

---
+++ classlar/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classlar
+{
+    internal class Customer
+    {
+        //field
+        //public string firstName;
+
+        //property
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string city{ get; set; }
+    }
+}
---
+++ classlar/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+
+            customerManager.Add();
+            customerManager.Update();
+
+            Customer customer = new Customer();
+            customer.city = "Ankara";
+            customer.Id = 1;
+            customer.firstName = "Cebiratül";
+            customer.lastName = "Garademlik Çayı";
+
+            //class girdilerini bu şekilde de yazabiliriz..
+            Customer customer2 = new Customer
+            {
+                Id = 2,
+                city = "Kayseri",
+                firstName = "Kestaneci",
+                lastName = "Gardaşlıkevlatları"
+            };
+
+            Console.WriteLine("Welcome " + customer.firstName + " " + customer.lastName);
+            Console.ReadKey();
+        }
+    } 
+}
